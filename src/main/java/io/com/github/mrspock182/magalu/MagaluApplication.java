cfef2eeb9529af3fed0bb91ecb2e4f11258a38fe
Kleber Nunes <mrspock182@MacBook-Pro-de-Kleber.local>
2020-10-15 20:10:01 -0300

    application start

---
+++ src/main/java/io/com/github/mrspock182/magalu/MagaluApplication.java
@@ -0,0 +1,13 @@
+package io.com.github.mrspock182.magalu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagaluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagaluApplication.class, args);
+	}
+
+}
